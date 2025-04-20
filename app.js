c38bd3a2067f7ef871e12b53b6dad882b3de1af4
ME.AT <meatish2008@gmail.com>
2025-04-20 22:02:28 +0330

    feat(app.js): add some basic stuff for route of the project

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 const express = require("express");
 const app = express();
+const authRouter = require("./routes/v1/auth");
 
-module.exports = app
+app.use("/v1/auth", authRouter);
+
+module.exports = app;
