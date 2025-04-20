17ce9258909a91b32899290430c959e7fe8b5646
ME.AT <meatish2008@gmail.com>
2025-04-20 21:56:50 +0330

    fix(validators/register.js): add phone to valiation prograss

--- validators/register.js
+++ validators/register.js
@@ -5,6 +5,7 @@ const schema = {
   name: { type: "string", min: 3, max: 255 },
   username: { type: "string", min: 3, max: 100 },
   email: { type: "email", min: 3, max: 100 },
+  phone: { type: "number" ,max: 11 },
   password: { type: "string", min: 5, max: 24 },
   confirmPassword: { type: "equal", field: "password" },
 };
