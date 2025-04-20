a38361dcaba7bb7b0627988248b3e08abc58c2b4
ME.AT <meatish2008@gmail.com>
2025-04-20 21:23:16 +0330

    feat(routes/auth.js): set some basic routes for auth

---
+++ routes/auth.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const controller = require("../controllers/auth.js");
+
+const router = express.Router();
+
+router.get("me", controller.getMe);
+
+router.post("/register", controller.register);
+router.post("/login", controller.login);
+
+
+module.exports = router
