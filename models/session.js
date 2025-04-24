8f406afde87f553b50debbd1023f1009edb6e68e
ME.AT <meatish2008@gmail.com>
2025-04-24 17:33:41 +0330

    feat(session.js): create model for session

---
+++ models/session.js
@@ -0,0 +1,33 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    time: {
+      type: String,
+      required: true,
+    },
+    free: {
+      type: Number,
+      required: true,
+    },
+    video: {
+      type: String,
+      required: true,
+    },
+    course: {
+      type: mongoose.Types.ObjectId,
+      ref: "Course",
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+const model = mongoose.model("session", schema);
+
+module.exports = model;
