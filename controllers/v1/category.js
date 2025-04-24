1e2b79a70d422bc4507512e75e97d206b02cee0e
ME.AT <meatish2008@gmail.com>
2025-04-24 14:29:28 +0330

    feat(category.js): create feature for create category

--- controllers/v1/category.js
+++ controllers/v1/category.js
@@ -1,4 +1,23 @@
-exports.create = async (req, res) => {};
+const categoryModel = require("../../models/category");
+const registerResult = require("../../validators/register");
+
+exports.create = async (req, res) => {
+  const validationResult = registerResult(req.body);
+
+  if (!validationResult) {
+    return res.status(422).json(validationResult);
+  }
+
+  const { title, href } = req.body;
+
+  const category = await categoryModel.create({ title, href });
+
+  if (!category) {
+    return res.status(500).json({ message: "Got error in server-side" });
+  }
+
+  return res.status(201).json({ category });
+};
 
 exports.getAll = async (req, res) => {};
 
