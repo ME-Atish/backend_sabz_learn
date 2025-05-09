ff2b5f537a9d1f081355046f6768f57f5298c4d9
ME.AT <meatish2008@gmail.com>
2025-05-09 14:51:41 +0330

    feat(course.js): create course model

---
+++ models/course.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    cover: {
+      type: String,
+      required: true,
+    },
+    support: {
+      type: String,
+      required: true,
+    },
+    href: {
+      type: String,
+      required: true,
+    },
+    categoryId: {
+      type: mongoose.Types.ObjectId,
+      ref: "category",
+    },
+    creator: {
+      type: mongoose.Types.ObjectId,
+      ref: "user",
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
+
+schema.virtual("session", {
+  ref: "session",
+  localField: "_id",
+  foreignField: "course",
+});
+schema.virtual("comment", {
+  ref: "comment",
+  localField: "_id",
+  foreignField: "course",
+});
+
+const model = mongoose.model("course", schema);
+
+module.exports = model;
