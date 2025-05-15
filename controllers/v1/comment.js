4d44c49aca5cbba3500f8bc8dc25439bacd06b86
ME.AT <meatish2008@gmail.com>
2025-05-15 13:37:29 +0330

    feat(comment.js): create feature that admins can delet comment

--- controllers/v1/comment.js
+++ controllers/v1/comment.js
@@ -1,3 +1,4 @@
+const { default: mongoose } = require("mongoose");
 const commentModel = require("../../models/comment");
 const courseModel = require("../../models/course");
 
@@ -20,4 +21,24 @@ exports.create = async (req, res) => {
   });
 
   return res.status(201).json({ comment });
+};
+
+exports.delete = async (req, res) => {
+  const isValidObjectId = mongoose.Types.ObjectId.isValid(req.params.id);
+
+  if (!isValidObjectId) {
+    return res.status(409).json({
+      message: "The id is not valid",
+    });
+  }
+
+  const deletedComment = await commentModel.findOneAndDelete({
+    _id: req.params.id,
+  });
+
+  if (!deletedComment) {
+    res.status(404).json({ message: "The comment not found" });
+  }
+
+  return res.status(200).json(deletedComment);
 };
--- routes/v1/comment.js
+++ routes/v1/comment.js
@@ -1,10 +1,13 @@
 const express = require("express");
 
 const authMiddleware = require("../../middlewares/auth.js");
+const isAdminMiddleware = require("../../middlewares/isAdmin.js");
 const commentController = require("../../controllers/v1/comment");
 
 const router = express.Router();
 
 router.route("/").post(authMiddleware , commentController.create)
+
+router.route("/:id").delete(authMiddleware, isAdminMiddleware , commentController.delete)
 
 module.exports = router;
